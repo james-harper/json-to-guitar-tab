49576eb5f544b732c5adc69476285da8c83d0ea5
James Harper <jameswilliamharper@gmail.com>
2017-08-03 00:20:51 +0100

    First commit

---
+++ js/app.js
@@ -0,0 +1,213 @@
+/**
+ * Convert sharps to the appropriate flat key
+ * to make lookups simpler
+ */
+function convertAccidental(key) {
+  if (key.length === 1) { return _.toUpper(key); }
+
+  let converted = _.toUpper(key.charAt(0));
+  if (key.charAt(1) === '#') {
+    converted = String.fromCharCode(converted.charCodeAt() + 1)
+    key = (converted < 'H') ? converted : 'A';
+
+    if (!['C', 'F'].includes(key)) {
+      key += 'b';
+    }
+  }
+
+  return key;
+};
+
+/**
+ * Find the name of a chord from its shape. Eg '-32010'
+ */
+const findByShape = function(target) {
+  let result = '?';
+
+  _(chordMap).each((chords,root) => {
+    _(chords).each((chord, tonality) => {
+      if (chord.length) {
+        _(chord).each(shape => {
+          if (shape === target) {
+            return result = root + ' ' + tonality;
+          }
+        })
+      }
+    })
+  });
+
+  return result;
+};
+
+/**
+ * Find chord shape from its name. Eg 'D major'
+ */
+const findByName = function(name) {
+  let splitBy = '_';
+  let root;
+  let tonality;
+  let extensions = [
+    7, 'dim', 'maj7', 'min7',
+  ];
+
+  name = name.replace(/\s/g, splitBy);
+
+  _(extensions).each(ext => {
+    if (name.includes(ext)) {
+      name = name.replace(splitBy, '');
+      root = name.split(ext)[0];
+      tonality = ext;
+    }
+  });
+
+  if (!root && !tonality) {
+    [root, tonality] = name.split(splitBy);
+  }
+
+  if (tonality === undefined) {
+    tonality = 'major';
+  }
+
+  root = convertAccidental(root);
+  return chordMap[root][tonality];
+};
+
+const strings = {
+  e: 'e',
+  B: 'B',
+  G: 'G',
+  D: 'D',
+  A: 'A',
+  E: 'E'
+};
+
+const constants = {
+  BEATS_PER_BAR: 4
+};
+
+function drawBar(chords, string, pattern, index) {
+  pattern = _.repeat(pattern, (constants.BEATS_PER_BAR * 4)/pattern.length);
+
+  let note = '-';
+  let output = '';
+
+  if (index % constants.BEATS_PER_BAR === 0) {
+    output += string + '|';
+  }
+
+  let multiplier = 1;
+  let currentChord = 0;
+
+  for (let i = 0; i < pattern.length; i++) {
+    let cutoff = Math.ceil(pattern.length / chords.length)
+
+    if (i >= cutoff * multiplier) {
+      currentChord++;
+      multiplier++;
+    }
+
+    let chord = chords[currentChord];
+
+    switch (string) {
+      case strings.e: note = chord[5]; break;
+      case strings.B: note = chord[4]; break;
+      case strings.G: note = chord[3]; break;
+      case strings.D: note = chord[2]; break;
+      case strings.A: note = chord[1]; break;
+      case strings.E: note = chord[0]; break;
+    }
+
+    // Use hex to represent higher frets
+    if (note !== '-' && note !== 'x') {
+      note = parseInt('0x'+note, 16);
+    }
+
+    let dontOutput = (note >= 10 && pattern[i-1] !== '-' && pattern[i-1] !== undefined);
+    if (!dontOutput) {
+      output += (pattern[i] !== '-') ? note : '-';
+    }
+  }
+
+  output += '|';
+
+  return output;
+}
+
+Vue.component('tab-bar', {
+  template:`
+  <span>
+    <span v-for="(chord, index) in bar.chords" style="font-size:11px">
+      {{ index > 0 ? ' | ' : ''}} {{findByShape(chord)}}
+    </span>
+    <span class="tab-line">{{draw(strings.e)}}</span>
+    <span class="tab-line">{{draw(strings.B)}}</span>
+    <span class="tab-line">{{draw(strings.G)}}</span>
+    <span class="tab-line">{{draw(strings.D)}}</span>
+    <span class="tab-line">{{draw(strings.A)}}</span>
+    <span class="tab-line">{{draw(strings.E)}}</span>
+  </span>
+  `,
+  props: ['bar', 'index'],
+  methods: {
+    draw(string) {
+      this.convertNamedtoShapes();
+      return drawBar(this.bar.chords, string, this.bar.pattern, this.index);
+    },
+    convertNamedtoShapes() {
+       // Convert any chords passed in by name into the approriate shape
+      this.bar.chords = this.bar.chords.map(chord => {
+        let shapes = [];
+
+        if (chord.includes('major')) shapes = findByName(chord);
+        if (chord.includes('minor')) shapes = findByName(chord);
+        if (chord.includes('maj7')) shapes = findByName(chord);
+        if (chord.includes('min7')) shapes = findByName(chord);
+        if (chord.includes(' 7')) shapes = findByName(chord);
+        if (chord.includes('dim')) shapes = findByName(chord);
+
+        if (shapes.length) {
+          let index = _.random(0, shapes.length - 1);
+          chord = shapes[index];
+        }
+
+        return chord;
+      });
+    }
+  },
+  created() {
+    this.convertNamedtoShapes();
+  }
+});
+
+Vue.component('tab',{
+  template: `
+  <div class="tab-section">
+    <span v-for="(chunk, index) in chunked" class="row">
+      <span v-for="(bar, index) in chunk" class="bar">
+        <tab-bar :index="index" :bar="bar"></tab-bar>
+      </span>
+    </span>
+  </div>
+  `,
+  props: ['progression'],
+  computed: {
+    chunked() {
+      return _.chunk(this.progression,constants.BEATS_PER_BAR);
+    }
+  }
+});
+
+new Vue({
+  el: "#app",
+  data: {
+    json: JSON.stringify(progression)
+  },
+  computed: {
+    progression() {
+      return JSON.parse(this.json);
+    }
+  },
+  created() {
+    this.json = JSON.stringify(JSON.parse(this.json), null, 2);
+  }
+});
---
+++ js/progressions.js
@@ -0,0 +1,74 @@
+const progression = [
+  {
+    chords: ['D major'],
+    pattern: 'x-----'
+  },
+  {
+    chords: ['577655'],
+    pattern: 'x---'
+  },
+  {
+    chords: ['B minor'],
+    pattern: 'x---'
+  },
+  {
+    chords: ['F# minor'],
+    pattern: 'x---'
+  },
+  {
+    chords: ['G major'],
+    pattern: 'x---xxxx----x-x-'
+  },
+  {
+    chords: ['D major'],
+    pattern: 'x---'
+  },
+  {
+    chords: ['G major'],
+    pattern: 'x---'
+  },
+  {
+    chords: ['A major'],
+    pattern: 'x-x-'
+  },
+];
+
+// vi–IV–I–V
+const progression_1 = [
+  {
+    chords: ["A minor"],
+    pattern: 'x---'
+  },
+  {
+    chords: ['F major'],
+    pattern: 'x---'
+  },
+  {
+    chords: ['C major'],
+    pattern: 'x---'
+  },
+  {
+    chords: ['G major', "E minor"],
+    pattern: 'x---'
+  }
+];
+
+// I-V-vi-IV
+const progression_2 = [
+  {
+    chords: ['-57775'],
+    pattern: 'x---'
+  },
+  {
+    chords: ['577655'],
+    pattern: 'x-x-'
+  },
+  {
+    chords: ['799777'],
+    pattern: 'x-x-'
+  },
+  {
+    chords: ['355433'],
+    pattern: 'x-x-'
+  }
+];
