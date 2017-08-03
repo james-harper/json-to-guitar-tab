e924be56752039235148b6707d262032aa29bf4a
James Harper <jameswilliamharper@gmail.com>
2017-08-03 19:13:50 +0100

    Added behave.js to make textarea react more intuitively

--- js/app.js
+++ js/app.js
@@ -257,18 +257,9 @@ new Vue({
   }
 });
 
-// Handle tabs in textarea
-(function(){
-var textareas = document.getElementsByTagName('textarea');
-var count = textareas.length;
-for(var i=0;i<count;i++){
-  textareas[i].onkeydown = function(e){
-    if(e.keyCode==9 || e.which==9){
-      e.preventDefault();
-      var s = this.selectionStart;
-      this.value = this.value.substring(0,this.selectionStart) + "\t" + this.value.substring(this.selectionEnd);
-      this.selectionEnd = s+1;
-    }
-  }
-}
-})()
+var editor = new Behave({
+    textarea: document.getElementById('jsonEditor'),
+    autoIndent: false,
+    tabSize: 2,
+    overwrite: false
+});
